bf506818ac3de59321f152e5becace6c93fb263a
Bohdan Dmytriv <dmytriv@quintagroup.org>
2018-04-25 11:38:54 +0300

    Fix utils tests

--- openprocurement/contracting/core/utils.py
+++ openprocurement/contracting/core/utils.py
@@ -1,8 +1,93 @@
 # -*- coding: utf-8 -*-
+from cornice.resource import resource
+from functools import partial
+from logging import getLogger
+from pkg_resources import get_distribution
+from pyramid.compat import decode_path_info
+from pyramid.exceptions import URLDecodeError
+from schematics.exceptions import ModelValidationError
+
 from openprocurement.api.utils import (
     apply_data_patch,
+    context_unpack,
+    error_handler,
     generate_id,
+    get_now,
+    get_revision_changes,
+    set_modetest_titles,
 )
+from openprocurement.api.models.auction_models.models import Revision
+from openprocurement.contracting.core.traversal import factory
+
+
+PKG = get_distribution(__package__)
+LOGGER = getLogger(PKG.project_name)
+
+contractingresource = partial(
+    resource,
+    error_handler=error_handler,
+    factory=factory
+)
+
+
+def extract_contract_adapter(request, contract_id):
+    db = request.registry.db
+    doc = db.get(contract_id)
+    if doc is not None and doc.get('doc_type') == 'contract':
+        request.errors.add('url', 'contract_id', 'Archived')
+        request.errors.status = 410
+        raise error_handler(request)
+    elif doc is None or doc.get('doc_type') != 'Contract':
+        request.errors.add('url', 'contract_id', 'Not Found')
+        request.errors.status = 404
+        raise error_handler(request)
+
+    return request.contract_from_data(doc)
+
+
+def extract_contract(request):
+    try:
+        # empty if mounted under a path in mod_wsgi, for example
+        path = decode_path_info(request.environ['PATH_INFO'] or '/')
+    except KeyError:
+        path = '/'
+    except UnicodeDecodeError as e:
+        raise URLDecodeError(e.encoding, e.object, e.start, e.end, e.reason)
+
+    contract_id = ""
+    # extract contract id
+    parts = path.split('/')
+    if len(parts) < 4 or parts[3] != 'contracts':
+        return
+
+    contract_id = parts[4]
+    return extract_contract_adapter(request, contract_id)
+
+
+def contract_from_data(request, data, raise_error=True, create=True):
+    contractType = data.get('contractType', 'common')
+    model = request.registry.contract_contractTypes.get(contractType)
+    if model is None and raise_error:
+        request.errors.add('data', 'contractType', 'Not implemented')
+        request.errors.status = 415
+        raise error_handler(request)
+    if model is not None and create:
+        model = model(data)
+    return model
+
+
+def contract_serialize(request, contract_data, fields):
+    contract = request.contract_from_data(contract_data, raise_error=False)
+    contract.__parent__ = request.context
+    return dict([(i, j) for i, j in contract.serialize("view").items() if i in fields])
+
+
+def save_contract(request):
+    """ Save contract object to database
+    :param request:
+    :return: True if Ok
+    """
+    contract = request.validated['contract']
 
 
 def save_contract(request):
