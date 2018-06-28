757d3d18c05008bd57efc2ab5ac77792704445bb
Andrew Leitsius <leits@quintagroup.com>
2018-06-28 09:18:46 +0300

    Add convoy permission to create contract

--- openprocurement/contracting/core/traversal.py
+++ openprocurement/contracting/core/traversal.py
@@ -16,6 +16,7 @@ class Root(object):
         (Allow, Everyone, 'view_contract'),
         (Allow, 'g:contracting', 'create_contract'),
         (Allow, 'g:brokers', 'create_contract'),
+        (Allow, 'g:convoy', 'create_contract'),
         (Allow, 'g:Administrator', 'edit_contract'),
         (Allow, 'g:brokers', 'edit_contract'),
         (Allow, 'g:admins', ALL_PERMISSIONS),
